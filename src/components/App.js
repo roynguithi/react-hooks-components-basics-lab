dbf6b93f668cac063cd8aadfb1b74648d9df05bb
roy collins nguithi <roynguithi@student.moringaschool.com>
2024-08-04 17:50:08 +0300

    react-hooks-components-basics-labs

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
     </nav>
   );
 }
@@ -16,15 +17,22 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+// About component
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+      <p>This is the about section of the website.</p>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
